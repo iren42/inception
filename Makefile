23bbcdd4baf9a2efad9d64617bea9ae1027dd90c
iren42 <iren@student.42.fr>
2024-01-09 21:52:07 +0100

    up: Erreur lors de la connexion a la base de donnees

--- Makefile
+++ Makefile
@@ -32,7 +32,7 @@ logs:
 .PHONY: prune
 prune:
 	docker stop $$(docker ps -qa)
-	docker rm $$(docker ps -qa)
+	docker rm -fv $$(docker ps -qa)
 	docker rmi -f $$(docker images -qa)
 	docker volume rm $$(docker volume ls -q)
 	docker network rm
--- srcs/requirements/wordpress/tools/setup.sh
+++ srcs/requirements/wordpress/tools/setup.sh
@@ -14,7 +14,18 @@ else
     wp core download --version=6.4.2 --locale=fr_FR --allow-root
     
     echo "config wordpress with cli"
-    wp config create --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=mariadb --allow-root
-    wp core install --url=https://${WP_ADM_USER}.42.fr --title="OLALA" --admin_user=${WP_ADM_USER} --admin_password=${WP_ADM_PASS} --admin_email="${WP_ADM_USER}"@42.fr --allow-root 
+    # Import env variables in the config file
+    cp /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
+	sed -i "s/votre_nom_de_bdd/$MYSQL_DATABASE/g" wp-config.php
+	sed -i "s/votre_utilisateur_de_bdd/$MYSQL_USER/g" wp-config.php
+	sed -i "s/votre_mdp_de_bdd/$MYSQL_PASSWORD/g" wp-config.php
+	sed -i "s/localhost/$MYSQL_HOST/g" wp-config.php
+	# Core installation needs for wordpress and admin user added
+#	wp core install --allow-root --url=${WP_URL} --title=${WP_TITLE} --admin_user=${WP_ADM_USER} --admin_password=${WP_ADM_PASS} --admin_email=${WP_ADM_EMAIL}
+	# Adds an extra user with no admin permissions
+#	wp user create --allow-root ${WP_USER} ${WP_USER_EMAIL} --user_pass=${WP_USER_PASS};
+#    wp config create --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=mariadb:3306 --allow-root
+#    wp core install --url=https://${WP_ADM_USER}.42.fr --title="OLALA" --admin_user=${WP_ADM_USER} --admin_password=${WP_ADM_PASS} --admin_email="${WP_ADM_USER}"@42.fr --allow-root 
     echo "done"
 fi
+exec /usr/sbin/php-fpm7.4 -F
