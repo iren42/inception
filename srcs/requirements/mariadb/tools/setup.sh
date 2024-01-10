6ace44660f8fa55517609cfa77633ba2d1f62741
iren42 <iren@student.42.fr>
2024-01-10 01:15:23 +0100

    up: database connexion is OK. WP shows its install.php page

--- srcs/requirements/mariadb/tools/setup.sh
+++ srcs/requirements/mariadb/tools/setup.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 
 echo "Setup database"
-SQL_FILE=/var/lib/mysql/.setup
+SQL_FILE=/var/lib/mysql/.dbissetup
 
 # Checks if the Data base with the given name already exists, if so it prints out a message and skips the else step
-if [ ! -e $SQL_FILE ];
+if [ -d $SQL_FILE ];
 then 
 	echo "Database already exists"
 else
 
-service mariadb start
+    service mariadb start
 
-echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;" | mysql -uroot
-echo "CREATE USER IF NOT EXISTS $MYSQL_USER@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" | mysql -uroot
-echo "GRANT ALL ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" | mysql -uroot
-echo "FLUSH PRIVILEGES;" | mysql -uroot
-#echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';" | mysql -uroot
-touch $SQL_FILE
+    echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;" | mysql -uroot
+    echo "CREATE USER IF NOT EXISTS $MYSQL_USER@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" | mysql -uroot
+    echo "GRANT ALL ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" | mysql -uroot
+    echo "FLUSH PRIVILEGES;" | mysql -uroot
+    #echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';" | mysql -uroot
+    touch $SQL_FILE
+    echo "database is setup"
+
+    # restart to make sure all our configurations are set in place
+    service mariadb stop
+    sleep 2
 fi
 
-sleep 2
-
-# stop the service
-# but execute it again later
-# to make sure all our configurations are set in place
-service mariadb stop
+exec mysqld_safe --bind-address=0.0.0.0
--- srcs/requirements/wordpress/tools/setup.sh
+++ srcs/requirements/wordpress/tools/setup.sh
@@ -1,31 +1,27 @@
 #!/bin/sh
 echo "running setup.sh for wordpress"
-#chown -R www-data:www-data /var/www/html
-#wait until database is ready
-#sleep 10
 
 #check if wp-config.php exist
 if [ -f /var/www/html/wp-config.php ]
 then
     echo "wordpress already downloaded"
 else
-    # create a new wp-config.php file
     echo "downloading wordpress with cli"
     wp core download --version=6.4.2 --locale=fr_FR --allow-root
     
     echo "config wordpress with cli"
-    # Import env variables in the config file
-#    cp /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
-#	sed -i "s/votre_nom_de_bdd/$MYSQL_DATABASE/g" wp-config.php
-#	sed -i "s/votre_utilisateur_de_bdd/$MYSQL_USER/g" wp-config.php
-#	sed -i "s/votre_mdp_de_bdd/$MYSQL_PASSWORD/g" wp-config.php
-#	sed -i "s/localhost/$MYSQL_HOST/g" wp-config.php
+    cp /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
+	sed -i "s/votre_nom_de_bdd/$MYSQL_DATABASE/g" wp-config.php
+	sed -i "s/votre_utilisateur_de_bdd/$MYSQL_USER/g" wp-config.php
+	sed -i "s/votre_mdp_de_bdd/$MYSQL_PASSWORD/g" wp-config.php
+	sed -i "s/localhost/$WP_HOST/g" wp-config.php
 	# Core installation needs for wordpress and admin user added
+#    wp core install --url=${WP_URL} --title=${WP_TITLE} --admin_user=${WP_ADM_USER} --admin_password=${WP_ADM_PASS} --admin_email="${WP_ADM_USER}"@42.fr --allow-root 
 #	wp core install --allow-root --url=${WP_URL} --title=${WP_TITLE} --admin_user=${WP_ADM_USER} --admin_password=${WP_ADM_PASS} --admin_email=${WP_ADM_EMAIL}
 	# Adds an extra user with no admin permissions
 #	wp user create --allow-root ${WP_USER} ${WP_USER_EMAIL} --user_pass=${WP_USER_PASS};
-    wp config create --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=mariadb:3306 --allow-root
-#    wp core install --url=${WP_URL} --title=${WP_TITLE} --admin_user=${WP_ADM_USER} --admin_password=${WP_ADM_PASS} --admin_email="${WP_ADM_USER}"@42.fr --allow-root 
-    echo "done"
+    echo "wordpress is ready"
+
 fi
+
 exec /usr/sbin/php-fpm7.4 -F
