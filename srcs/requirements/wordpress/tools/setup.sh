045b3b8d7b3f9b076e302558b705866745868b80
iren42 <iren@student.42.fr>
2024-01-21 17:18:03 +0100

    cleaning

--- srcs/requirements/wordpress/tools/setup.sh
+++ srcs/requirements/wordpress/tools/setup.sh
@@ -9,14 +9,12 @@ if [ -s wp-config.php ]; then
 else
     echo "config wordpress manual/cli"
     wp config create --path=/var/www/wordpress \
-                            --allow-root \
                             --dbname=$MYSQL_DATABASE \
                             --dbuser=$MYSQL_USER \
                             --dbpass=$MYSQL_PASSWORD \
                             --dbhost=$WP_HOST
 	# Core installation needs for wordpress and admin user added
     wp core install --path=/var/www/wordpress \
-                            --allow-root \
                             --url=$WP_URL \
                           --title=$WP_TITLE \
                           --admin_user=$WP_ADM_USER \
@@ -24,7 +22,6 @@ else
                           --admin_email=$WP_ADM_EMAIL
 	# Adds an extra user with no admin permissions
 	wp user create --path=/var/www/wordpress \
-                            --allow-root \
                           ${WP_USER} \
                           ${WP_USER_EMAIL} \
                           --user_pass=${WP_USER_PASS}
